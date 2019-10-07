7655f9010d4d1d78d7a24b90abfdb68a5643c284
musheer1999 <musheerahmad.710@gmail.com>
2019-10-07 23:25:38 +0530

    hospital

---
+++ app.js
@@ -0,0 +1,57 @@
+function dataSearch(form){
+var data=form.data.value;
+var name1="Vivek Sharma";
+var id1="JI7560";
+var name2="Rahul Gupta";
+var id2="JF7860";
+var name3="Zaid Khan";
+var id3="JT7890";
+var name4="Yash Gupta";
+var id4="JF7890";
+var name5="Tazeem Khan";
+var id5="JT7880";
+var name6="Yash Varshney";
+var id6="JF7440";
+var name7="Saad Khan";
+var id7="JT7830";
+var name8="Manish Gupta";
+var id8="JK7860";
+var name9="Farhan Khan";
+var id9="JR7820";
+var name10="Kunal Sharma";
+var id10="JS7860";
+
+if(data==name1 || data==id1){
+window.location="Vivek_Sharma.html";
+}
+else if(data==name2 || data==id2 ){
+window.location="Rahul_Gupta.html";
+}
+else if(data==name3 || data==id3 ){
+window.location="Zaid_Khan.html";
+}
+else if(data==name4 || data==id4 ){
+window.location="Yash_Gupta.html";
+}
+else if(data==name5 || data==id5 ){
+window.location="Tazeem_Khan.html";
+}
+else if(data==name6 || data==id6 ){
+window.location="Yash_Varshney.html";
+}
+else if(data==name7 || data==id7 ){
+window.location="Saad_Khan.html";
+}
+else if(data==name8 || data==id8 ){
+window.location="Manish_Gupta.html";
+}
+else if(data==name9 || data==id9 ){
+window.location="Farhan_Khan.html";
+}
+else if(data==name10 || data==id10 ){
+window.location="Kunal_Sharma.html";
+}
+else{
+alert("No Record Found!");
+}
+}
